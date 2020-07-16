293fdb2837f7e208bfda5d70224840b774014b9f
Cristian Ortega <orte.ca1208@gmail.com>
2020-07-16 00:07:30 -0400

    CRUD

---
+++ proyecto/src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import {Employee} from './models/employee';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  employeeArray: Employee[] = [
+    {id: 1, name: "Cristian", country: "PY"},
+    {id: 2, name: "Ryan", country: "USA"},
+    {id: 3, name: "Lautaro", country: "ARG" }
+  ];
+  selectedEmployee: Employee = new Employee();
+
+  openForEdit(employee: Employee){
+    this.selectedEmployee = employee;
+  }
+
+  addOrEdit(){
+    if(this.selectedEmployee.id === 0){
+      this.selectedEmployee.id = this.employeeArray.length + 1;
+      this.employeeArray.push(this.selectedEmployee);
+    }
+    this.selectedEmployee = new Employee();
+  }
+    delete(){
+    if(confirm('Are you sure you want to delete it?')){
+      this.employeeArray = this.employeeArray.filter(x => x != this.selectedEmployee);
+      this.selectedEmployee = new Employee();
+    }
+    
+  } 
+}
